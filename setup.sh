75d8bc5eb109281cf4632a7149f135517ed4ab60
Amro Diab <adiab@hotmail.co.uk>
2022-12-03 18:44:14 +0000

    ensure we have the correct mailmerge package installed

--- setup.sh
+++ setup.sh
@@ -5,4 +5,7 @@
 apt update
 apt install python3 python3-pip pandoc texlive-latex-base texlive-fonts-recommended texlive-latex-extra
 locale-gen en_GB.UTF-8
+pip3 install -r requirements.txt
+pip3 uninstall mailmerge
+pip3 install docx-mailmerge
 update-locale
